6d99bceff9a05469c203c48dc1359dd271338a07
Claudiu Creanga <danutempyrium@yahoo.com>
2018-11-07 17:11:15 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
-import "fmt"
+import (
+        flag "github.com/jessevdk/go-flags"
+        //"fmt"
+        //"os"
+        //"strings"
+)
+
+
+type Defaults struct {
+        Verbose []bool `short:"v" long:"verbose" description:"Show verbose    debug information"`
+        Terse   bool   `short:"t" long:"terse" description:"Shows terse output"`
+}
+
+type ListCommand struct {
+        Config string `short:"c" long:"config" description:"config file" optional:"yes"`
+}
+
 func main() {
-	fmt.Println("Hello, World")
+        defaultOptions := Defaults{}
+        listCmd := ListCommand{}
+
+        parser := flag.NewParser(&defaultOptions, flag.Default)
+        parser.AddCommand("list", "lists something", "", &listCmd)
+
+        parser.Parse()
 }
