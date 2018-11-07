bbf7ec2b1574f7ecc1797908fd01e96cab24e552
Claudiu Creanga <danutempyrium@yahoo.com>
2018-11-07 23:22:02 +0000

    init

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 import (
         flag "github.com/jessevdk/go-flags"
-        //"fmt"
+        "fmt"
         //"os"
         //"strings"
+	"os"
 )
 
 
@@ -12,16 +13,30 @@ type Defaults struct {
         Terse   bool   `short:"t" long:"terse" description:"Shows terse output"`
 }
 
-type ListCommand struct {
-        Config string `short:"c" long:"config" description:"config file" optional:"yes"`
+type options struct {
+        Config string `short:"c" long:"config" description:"config file"`
+		Help     bool     `short:"h" long:"help" description:"show help message"`
 }
 
 func main() {
         defaultOptions := Defaults{}
-        listCmd := ListCommand{}
+        opts := options{}
 
         parser := flag.NewParser(&defaultOptions, flag.Default)
-        parser.AddCommand("list", "lists something", "", &listCmd)
+        parser.AddCommand("trash", "manage trash", "", &opts)
 
         parser.Parse()
-}
+        fmt.Println(opts.Config)
+
+		_, err := parser.Parse()
+		if err != nil {
+			fmt.Printf("fail to parse args: %v", err)
+			os.Exit(1)
+		}
+
+		if opts.Help {
+			parser.WriteHelp(os.Stdout)
+			os.Exit(0)
+		}
+
+        }
